7212d96c0aa918bf4dd1936f94438910b84cd18b
Vijayendra <vijayendra.c.o.o.l@gmail.com>
2012-09-12 10:58:39 +0530

    First commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+This is a e commerce website test demo set up using spree . I am going to set up a similiar e commerce site really soon . 
+To do:
+
+Study and modify spree :)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trader::Application.config.secret_token = 'f5bb49636110a03ea94be0ef97ba3843dcf605db09c6330bc5ab487ac7b27f990d98f87d891126e271388de2c9ee3ace1bbafe66acd20ce0fc0a6fdb627c424f'
